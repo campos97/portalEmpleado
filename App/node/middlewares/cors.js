d56e0d4abe057f622b3d951439e4443de4a2229e
campos97 <105729408+campos97@users.noreply.github.com>
2024-06-23 14:07:28 +0200

    Delete App/node directory

--- App/node/middlewares/cors.js
+++
@@ -1,9 +0,0 @@
-import cors from "cors";
-
-const corsOptions = {
-  origin: "*", // Configura esto según tus necesidades
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-};
-
-export default cors(corsOptions);
